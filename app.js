230f3087775aea78a2c4f952d08230e7ac9c2c7e
cahyof30 <f30.cahyo@gmail.com>
2025-08-08 15:21:51 +0700

    1st commit

---
+++ app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Pesan dari Entpoint Express",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+// CONNECTION
+mongoose.connect(process.env.DATABASE, {}).then(() => {
+  console.log("Database Connect!");
+});
---
+++ client/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Tes 123</h1>
+</template>
+
+<script setup>
+import axios from "axios";
+import { ref, onMounted } from "vue";
+
+const dataMessage = ref(null);
+
+const getMessage = async () => {
+  try {
+    const response = await axios.get("http://localhost:3000");
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+onMounted(() => {
+  getMessage();
+});
+</script>
